f4ea05693db150672c3f9699db7134dd97c0f3b6
Skomaiya <s.komaiya@alustudent.com>
2025-01-07 08:21:49 +0000

    task 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -5,7 +5,7 @@ This script demonstrates the use of variable annotations in Python.
 Variables:
     a (int): An integer variable.
     pi (float): A floating-point variable representing the value of pi.
-    i_understand_annotations (bool): A boolean variable indicating understanding of annotations.
+    i_understand_annotations (bool): A boolean variable.
     school (str): A string variable containing the name of the school.
 """
 
