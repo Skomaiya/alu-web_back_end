e85e41c5b94a27f582d8a1816aa27e027df6ff48
Skomaiya <s.komaiya@alustudent.com>
2025-01-07 08:33:36 +0000

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 """
-This script defines a function to compute the sum of a list of floating-point numbers.
+This script defines a type-annotated function to compute the sum of a list of floating-point numbers.
 
 Functions:
     sum_list(input_list): Returns the sum of all elements in a list of floats.
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """
     Computes the sum of all elements in a list of floating-point numbers.
 
     Args:
-        input_list (list[float]): A list of floating-point numbers to sum.
+        input_list (List[float]): A list of floating-point numbers to sum.
 
     Returns:
         float: The sum of all elements in the input list.
     """
-    float_sum = 0.0  # Initialize the sum to zero
-    for number in input_list:
-        float_sum += number
+    float_sum = sum(input_list)
     return float_sum
