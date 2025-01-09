53970e71f8a2ed4e3978fb2c55af27ef5eea9ad1
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-01-09 13:32:30 +0200

    Add new line to end of file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -16,5 +16,5 @@ async def async_generator() -> Generator[float, None, None]:   # type: ignore
         int: A random number between 0 and 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)  # Wait for 1 second asynchronously
-        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
