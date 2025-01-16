dfc748c44fa94964152da7dbbcd1c9bec5f24a4f
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-01-16 15:05:30 +0200

    Create README file and complete Task 1.

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""filter logger"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
