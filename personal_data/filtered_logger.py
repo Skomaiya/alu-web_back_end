ede1738492c0cc20550e3fda6349a80ceaf7e45f
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-01-16 15:15:31 +0200

    Complete Task 4

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -45,3 +45,43 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         password=password,
         host=host,
         database=db_name)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+
+def main():
+    """main def"""
+    db = get_db()
+    cursor = db.cursor()
+    logger = get_logger()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        msg = f"name={row[0]}; email={row[1]}; phone={row[2]}; \
+            ssn={row[3]}; password={row[4]}; ip={row[5]}; \
+                last_login={row[6]}; \
+                user_agent={row[7]};"
+        logger.info(msg)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
