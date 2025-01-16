508634186aaf3ebba0925046435ab8c4297df124
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-01-16 15:18:01 +0200

    Update encrypt_password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,8 +7,3 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ encrypt password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ check if password is valid """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
