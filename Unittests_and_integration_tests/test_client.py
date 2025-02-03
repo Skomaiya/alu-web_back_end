33f152e9434d3358473052a69c66245bc774f32a
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-02-03 20:16:22 +0200

    Retry Task 6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from parameterized import parameterized, parameterized_class
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
