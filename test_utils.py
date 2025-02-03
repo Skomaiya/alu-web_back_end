dab97959d9bbd2c8d07e9feba60dd55d06544ac6
Komaiya Samuel <133634554+Skomaiya@users.noreply.github.com>
2025-02-03 19:44:52 +0200

    Attempt Task 0

---
+++ test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Testing utils.py """
+
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test for access_nested_map"""
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test for access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test for Keyerror exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
